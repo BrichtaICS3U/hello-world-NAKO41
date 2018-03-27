277a5bff2f99728b8d3e1f7dbaac53868f024836
NSPro4w <nspro1@ocdsb.ca>
2018-03-27 09:18:45 -0400

    cubeified

--- Newton's method.py
+++ Newton's method.py
@@ -1,6 +1,6 @@
 #Newton's, method
 
-def Newton(x,g,accuracy = 0.001):
+def Newton2(x,g,accuracy = 0.001):
     """
     implement a solution to Newton's method for finding square roots,
     which is a little different than Heron's method.
@@ -10,7 +10,7 @@ def Newton(x,g,accuracy = 0.001):
     """
     
 
-
+    #^2
     while abs(((g**2)- x)) > accuracy:
         g = g - (((g**2)-x)/(2*g))
 
@@ -20,3 +20,16 @@ def Newton(x,g,accuracy = 0.001):
 #i personally think that Newton's method would be a more efficient method
 #mainly because it is able to find cube roots and not just square roots like
 #heron's method.
+
+
+
+def Newton3(x,g,accuracy = 0.001):
+    """the goal is thesame as the above function but for cube roots
+    """
+
+
+    #^3
+    while abs(((g**3)-x)) > accuracy:
+        g = g - (((g**3)-x)/(3*g**2))
+
+    return g
